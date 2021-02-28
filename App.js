bb8255f67e3a4a0b684f7df6d78bff4b1e92f865
francisco javier <ffuentes@ucol.mx>
2021-02-28 01:35:44 -0600

    added controll buttons. display song time and id. refactor needed.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   StatusBar,
@@ -6,17 +6,28 @@ import {
   Text,
   View,
   PermissionsAndroid,
+  TouchableOpacity,
 } from 'react-native';
-import TrackPlayer from 'react-native-track-player';
+import TrackPlayer, {
+  useTrackPlayerProgress,
+  useTrackPlayerEvents,
+  TrackPlayerEvents,
+} from 'react-native-track-player';
 import MusicFiles, {Constants} from 'react-native-get-music-files-v3dev-test';
+// docs: https://www.npmjs.com/package/@fortawesome/react-native-fontawesome
+// icons: https://fontawesome.com/icons?d=gallery&p=2&c=audio-video
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 
-// import fontawesome icons, only in app.js
-// https://www.npmjs.com/package/@fortawesome/react-native-fontawesome
-// https://fontawesome.com/icons?d=gallery&p=2&c=audio-video
+// import icons, only in app.js
 import {library} from '@fortawesome/fontawesome-svg-core';
-import {faPause, faPlay, faCoffee} from '@fortawesome/free-solid-svg-icons';
-library.add(faPause, faPlay, faCoffee);
+import {
+  faPause,
+  faPlay,
+  faEllipsisH,
+  faStepForward,
+  faStepBackward,
+} from '@fortawesome/free-solid-svg-icons';
+library.add(faPause, faPlay, faEllipsisH, faStepForward, faStepBackward);
 
 const App = () => {
   const requestFilesPermission = async () => {
@@ -41,7 +52,7 @@ const App = () => {
 
   const setupTrackplayer = async () => {
     try {
-      let {length, songs} = await getAllSongs();
+      let {length, results: songs} = await getAllSongs();
       songs = songs.map((song) => ({...song, url: song.path}));
       console.log('tracks: ', length);
       await TrackPlayer.setupPlayer();
@@ -64,6 +75,13 @@ const App = () => {
     }
   };
 
+  const [currentSongIdStr, setcurrentSongIdStr] = useState(null);
+  useTrackPlayerEvents([TrackPlayerEvents.PLAYBACK_TRACK_CHANGED], (event) => {
+    console.log('event.', event);
+    setcurrentSongIdStr(event.nextTrack);
+  });
+
+  // on mount
   useEffect(() => {
     requestFilesPermission();
     setupTrackplayer();
@@ -73,37 +91,86 @@ const App = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const secondsToTimeString = (seconds) => {
+    let min = Math.floor(seconds / 60);
+    let sec = Math.floor(seconds - min * 60);
+
+    if (min < 10) {
+      min = '0' + min;
+    }
+    if (sec < 10) {
+      sec = '0' + sec;
+    }
+
+    return `${min}:${sec}`;
+  };
+
+  const {position, duration} = useTrackPlayerProgress();
+  const percentageProgress = `${(position * 100) / duration}%`;
+
   return (
     <>
       <StatusBar />
       <SafeAreaView style={[styles.SafeAreaView, styles.fill]}>
         <View style={styles.container}>
           <View style={[styles.container, styles.fill]}>
-            <Text style={styles.title}>ES lytic</Text>
+            <Text style={styles.title}>{secondsToTimeString(duration)}</Text>
           </View>
           <View style={styles.nusicData}>
-            <Text style={styles.title}>artist - song</Text>
+            <Text style={styles.songName}>
+              {`current track:'${currentSongIdStr}'`}
+            </Text>
+            <FontAwesomeIcon
+              style={styles.FontAwesomeIcon}
+              size={22}
+              icon="ellipsis-h"
+            />
           </View>
           <View style={[styles.container, styles.fill]}>
-            <Text style={styles.title}>EN lytic</Text>
+            <Text style={styles.title}>{secondsToTimeString(position)}</Text>
           </View>
         </View>
+
+        <View style={styles.progresBar}>
+          <View style={[styles.progress, {width: percentageProgress}]} />
+        </View>
         <View style={styles.playerControls}>
-          <FontAwesomeIcon
-            style={styles.FontAwesomeIcon}
-            size={25}
-            icon="pause"
-          />
-          <FontAwesomeIcon
-            style={styles.FontAwesomeIcon}
-            size={25}
-            icon="coffee"
-          />
-          <FontAwesomeIcon
-            style={styles.FontAwesomeIcon}
-            size={25}
-            icon="play"
-          />
+          <TouchableOpacity
+            onPress={() => TrackPlayer.skipToPrevious()}
+            style={styles.btnWraper}>
+            <FontAwesomeIcon
+              style={styles.FontAwesomeIcon}
+              size={25}
+              icon="step-backward"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => TrackPlayer.pause()}
+            style={styles.btnWraper}>
+            <FontAwesomeIcon
+              style={styles.FontAwesomeIcon}
+              size={25}
+              icon="pause"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => TrackPlayer.play()}
+            style={styles.btnWraper}>
+            <FontAwesomeIcon
+              style={styles.FontAwesomeIcon}
+              size={25}
+              icon="play"
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => TrackPlayer.skipToNext()}
+            style={styles.btnWraper}>
+            <FontAwesomeIcon
+              style={styles.FontAwesomeIcon}
+              size={25}
+              icon="step-forward"
+            />
+          </TouchableOpacity>
         </View>
       </SafeAreaView>
     </>
@@ -120,19 +187,35 @@ const styles = StyleSheet.create({
   SafeAreaView: {
     display: 'flex',
     flexDirection: 'column',
-    backgroundColor: 'black',
   },
   container: {
     display: 'flex',
     flex: 9,
     flexDirection: 'column',
-    backgroundColor: 'black',
+    backgroundColor: '#151515',
     justifyContent: 'center',
     alignItems: 'center',
   },
   nusicData: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 13,
+    paddingVertical: 1,
     backgroundColor: '#b0b0b0',
     width: '96%',
+    borderRadius: 2,
+  },
+  progresBar: {
+    display: 'flex',
+    height: 4,
+    backgroundColor: 'white',
+    justifyContent: 'flex-start',
+  },
+  progress: {
+    height: '100%',
+    backgroundColor: '#6e89f5',
   },
   playerControls: {
     display: 'flex',
@@ -144,15 +227,17 @@ const styles = StyleSheet.create({
     paddingLeft: 60,
     paddingRight: 60,
   },
-  btn: {
-    borderRadius: 15,
-    backgroundColor: 'red',
-    width: 200,
-    padding: 5,
+  btnWraper: {
+    padding: 6,
   },
   title: {
     fontSize: 24,
     fontWeight: '800',
+    color: 'white',
+    textAlign: 'center',
+  },
+  songName: {
+    fontSize: 20,
     color: 'white',
     textAlign: 'center',
   },
