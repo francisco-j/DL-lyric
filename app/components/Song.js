fd68173d2517874e672e959dc552a4c4d46c5587
francisco-j <ffuentes@ucol.mx>
2021-03-31 00:07:17 -0600

    SongList Screen

---
+++ app/components/Song.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import TrackPlayer from 'react-native-track-player';
+
+// BackBtn - functional component
+export default ({navigation, track}) => {
+  return (
+    <View style={St.container}>
+      <TouchableOpacity
+        style={St.backBtn}
+        onPress={() => {
+          TrackPlayer.skip(track.id)
+          TrackPlayer.play()
+          navigation.navigate('LyricVew')
+        }}
+      >
+        <FontAwesomeIcon
+          style={{color: 'white', margin: 10}}
+          size={40}
+          icon="play"
+        />
+      </TouchableOpacity>
+      <View style={St.info}>
+        <Text style={St.title}>{track.title || 'unknown'}</Text>
+        <Text style={St.artist}>{track.artist || 'unknown'}</Text>
+      </View>
+    </View>
+  )
+}
+
+const St = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: 10,
+    width: '100%'
+  },
+  info: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  title: {
+    color: 'white',
+    fontSize: 24,
+  },
+  artist: {
+    color: 'white',
+    fontSize: 24,
+  },
+});
+
--- app/screens/SongsList.js
+++ app/screens/SongsList.js
@@ -1,57 +1,65 @@
-import React from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, {useEffect, useState}  from 'react';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import TrackPlayer from 'react-native-track-player';
+import Song from '../components/Song';
 
 // LyricVew - functional component
 export default ({navigation}) => {
-  return (
-    <View style={[St.container, St.main]}>
-      <Text style={St.text}>here goes the list of songs</Text>
-      <TouchableOpacity
-        style={St.btn}
-        onPress={() =>{
-          navigation.navigate('LyricVew')
-        }}
-      >
-        <Text style={St.text}>go to lrc</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={St.btn}
-        onPress={() =>{
-          navigation.navigate('Info')
-        }}
-      >
-        <Text style={St.text}>info</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={St.btn}
-        onPress={() =>{
-          navigation.navigate('Help')
-        }}
+  let [queue, setQueue] = useState([])
 
-        >
-        <Text style={St.text}>help</Text>
-      </TouchableOpacity>
+  useEffect(() => {
+    async function asyncEffectBehavior() {
+      let Q = await TrackPlayer.getQueue();
+      setQueue(Q)
+    }
+    asyncEffectBehavior()
+  }, []);
+
+  const renderSongs = () => queue.map(track =>
+    <Song navigation={navigation} track={track} key={track.id}/>
+  );
+
+  return (
+    <View style={[St.container]}>
+      <View style={[St.icons]}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Info')}>
+          <FontAwesomeIcon
+            style={{color: 'white', marginHorizontal: 15}}
+            size={26}
+            icon="info-circle"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Help')}>
+          <FontAwesomeIcon
+            style={{color: 'white', marginHorizontal: 5}}
+            size={26}
+            icon="question"
+          />
+        </TouchableOpacity>
+      </View>
+
+      {renderSongs()}
     </View>
   );
 };
 
 const St = StyleSheet.create({
-  main: {
-    flex: 9,
-  },
   container: {
     display: 'flex',
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#151515',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
+    paddingTop: 20
   },
-  btn: {
-    margin: 10,
-    padding: 10,
-    borderRadius: 5,
-    backgroundColor: 'red',
+  icons: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginVertical: 20
   },
   text: {
     color: '#fff'
